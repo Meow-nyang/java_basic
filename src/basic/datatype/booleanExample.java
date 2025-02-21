911686c6a202c1baea1af1c1ed3c0919da484ccb
Meow_nyang <slah76@naver.com>
2025-02-21 15:22:06 +0900

    Day01 - 다양한 기본 데이터 타입

---
+++ src/basic/datatype/booleanExample.java
@@ -0,0 +1,14 @@
+package basic.datatype;
+
+public class booleanExample {
+
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+//        boolean b3 = True; 대문자 X
+//        boolean b4 = False;
+//        boolean b5 = 0; X
+
+    }
+
+}
