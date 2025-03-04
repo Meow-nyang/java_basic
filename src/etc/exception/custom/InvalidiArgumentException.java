74f98d83ed4a466e226c9340a49a4076f2b95453
Meow_nyang <slah76@naver.com>
2025-03-04 23:59:51 +0900

    Day07 - custom 예외 클래스

---
+++ src/etc/exception/custom/InvalidiArgumentException.java
@@ -0,0 +1,9 @@
+package etc.exception.custom;
+
+public class InvalidiArgumentException extends Exception {
+
+    public InvalidiArgumentException(String message){
+        super(message);
+
+    }
+}
