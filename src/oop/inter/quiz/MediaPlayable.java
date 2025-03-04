5b8eafb052738d2ae1129ee03a0df05888bb47d6
Meow_nyang <slah76@naver.com>
2025-03-04 11:10:24 +0900

    Day07 - 연습문제 풀이(poly, inter)

---
+++ src/oop/inter/quiz/MediaPlayable.java
@@ -0,0 +1,9 @@
+package oop.inter.quiz;
+
+public interface MediaPlayable {
+
+    void play();
+    void pause();
+    void stop();
+
+}
---
+++ src/oop/inter/quiz/MediaPlayer.java
@@ -0,0 +1,30 @@
+package oop.inter.quiz;
+
+public class MediaPlayer {
+
+    private MediaPlayable[] mediaList;
+
+    public MediaPlayer() {
+        this.mediaList = new MediaPlayable[0];
+    }
+
+    public  void addMedia(MediaPlayable media) {
+        MediaPlayable[] temp = new MediaPlayable[mediaList.length + 1];
+        for (int i = 0; i < mediaList.length; i++) {
+            temp[i] = mediaList[i];
+        }
+        temp[temp.length-1] = media;
+        mediaList = temp;
+
+
+    }
+
+    public void playAll() {
+        for (MediaPlayable playable : mediaList) {
+            playable.play();
+        }
+
+    }
+
+
+}
